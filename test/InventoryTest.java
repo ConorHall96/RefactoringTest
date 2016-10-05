678bd9a5f7e9eb0290d9769b0c253dfa2c8f7cbb
t00178904 <conorhalll.96@gmail.com>
2016-10-05 12:38:31 +0100

    Initial Commit, Created test classes

---
+++ test/InventoryTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by t00178904 on 05/10/2016.
+ */
+public class InventoryTest {
+    @Test
+    public void addGuitar() throws Exception {
+
+    }
+
+    @Test
+    public void getGuitar() throws Exception {
+
+    }
+
+    @Test
+    public void search() throws Exception {
+
+    }
+
+}
